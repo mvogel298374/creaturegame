71a072077dff31f8700ab5bfa6f61863bf9edf38
mvogel298374 <93988847+mvogel298374@users.noreply.github.com>
2026-04-07 22:58:02 +0200

    git versioning

---
+++ PokeApiConnector/PokeAPI/PokeApiMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PokeApiConnector.PokeAPI;
+
+public class PokeApiMove
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("accuracy")]
+    public int? Accuracy { get; set; }
+
+    [JsonPropertyName("pp")]
+    public int? Pp { get; set; }
+
+    [JsonPropertyName("power")]
+    public int? Power { get; set; }
+
+    [JsonPropertyName("damage_class")]
+    public NamedApiResource DamageClass { get; set; }
+
+    [JsonPropertyName("type")]
+    public NamedApiResource Type { get; set; }
+
+    [JsonPropertyName("effect_entries")]
+    public List<EffectEntry> EffectEntries { get; set; }
+}
+
+public class NamedApiResource
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
+
+public class EffectEntry
+{
+    [JsonPropertyName("effect")]
+    public string Effect { get; set; }
+
+    [JsonPropertyName("short_effect")]
+    public string ShortEffect { get; set; }
+
+    [JsonPropertyName("language")]
+    public NamedApiResource Language { get; set; }
+}
---
+++ PokeApiConnector/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PokeApiConnector;
+using PokeApiConnector.PokeAPI;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await MoveImport.FetchMovesByGeneration(1);
+        //await MoveImport.FetchMoveData(15);
+    }
+}
