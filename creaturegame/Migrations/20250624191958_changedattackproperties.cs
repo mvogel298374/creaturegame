6eed59a9002731c84ddc274c0e0a91ee156bfed7
mvogel298374 <93988847+mvogel298374@users.noreply.github.com>
2026-04-07 23:00:13 +0200

    just all of it

---
+++ creaturegame/Migrations/20250624191958_changedattackproperties.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace creaturegame.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedattackproperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AttackType",
+                table: "Moves",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttackType",
+                table: "Moves");
+        }
+    }
+}
